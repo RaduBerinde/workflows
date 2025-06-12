8392d74b0aa9b03cf23647c47a2a5c8e994639ba
Radu Berinde <radu@cockroachlabs.com>
2025-06-12 12:04:11 -0700

    post slack

--- scripts/check-pebble-dep.sh
+++ scripts/check-pebble-dep.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-echo FAIL
+echo  Branch release-23.2 pebble dependency not up to date: 7cc65fb51ef2 vs current 7cc65fb51ef2
+echo  Branch release-24.1 pebble dependency not up to date: d66731e169ea vs current d66731e169ea
+echo  Branch release-24.3 pebble dependency up to date.
+echo  Branch release-25.1 pebble dependency up to date.
+echo  Branch release-25.2 pebble dependency up to date.
+echo  Branch master pebble dependency up to date.
 exit 1
