cf362305de8c79ea2447a1f0cbe8fa24772be83e
Radu Berinde <radu@cockroachlabs.com>
2025-06-12 18:44:28 -0700

    update

--- scripts/check-pebble-dep.sh
+++ scripts/check-pebble-dep.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-echo  Branch release-23.2 pebble dependency not up to date: 7cc65fb51ef2 vs current 7cc65fb51ef2
-echo  Branch release-24.1 pebble dependency not up to date: d66731e169ea vs current d66731e169ea
-echo  Branch release-24.3 pebble dependency up to date.
-echo  Branch release-25.1 pebble dependency up to date.
-echo  Branch release-25.2 pebble dependency up to date.
-echo  Branch master pebble dependency up to date.
+echo "Some release branches have out-of-date Pebble dependencies:"
+echo "  - release-24.2: dependency set at ee4c9351eb11, but crl-release-24.2 tip is 358867e65e96"
+echo "  - release-24.3: dependency set at 2b6d7dd769b3, but crl-release-24.3 tip is f4be12ca381c"
+echo "  - release-25.1: dependency set at cd24d729c91d, but crl-release-25.1 tip is c9773006c1d9"
+echo "  - release-25.2: dependency set at 4f5d42f6d69f, but crl-release-25.2 tip is d3d7d059487e"
 exit 1
